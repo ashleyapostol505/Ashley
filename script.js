3aac7a43bff3b6577549114a2b38d086ba949ce1
ASHLEY APOSTOL <149588816+ashleyapostol505@users.noreply.github.com>
2023-11-06 20:20:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front end", "Web Developer", "Caring", "", ""],
+        strings: ["Expressive", "Intuitive", "Engaging", "", ""],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
